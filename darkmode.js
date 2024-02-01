812ef1df900815465f53ca9795cf8b8955f65924
Blue-Robin-Taken <86581171+Blue-Robin-Taken@users.noreply.github.com>
2024-02-01 17:33:09 +0000

    lol

--- darkmode.js
+++ darkmode.js
@@ -1,34 +1,55 @@
 var darkmode = false;
 
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
 function changeToDark(e, index, num){
     e.style.color="white";
     if (e.constructor == HTMLAnchorElement){
-        e.style.color="#03fcc6"
+        e.style.color="#03fcc6";
     }
 
 }
 function darkToLight(e, index, num){
-    e.style.color=""
+    e.style.color="";
 }
 
 window.onload = function(){ // https://stackoverflow.com/questions/9778888/uncaught-typeerror-cannot-set-property-onclick-of-null
-    document.getElementById("darkmode").onclick = function() {switchDarkOrLight()};
     var darkmodeImage = document.getElementById("darkmodeimage");
-    var darkmode = false;
-    function switchDarkOrLight() {
-    darkmode = !darkmode
-    
 
-    hOnes = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, a");
-    if (darkmode){
+    if (getCookie('darkmode')==null){
+        document.cookie = "darkmode=false;";
+    }
+
+    document.getElementById("darkmode").onclick = function() {switchDarkOrLight()};
+    if (getCookie('darkmode')=='true'){
         document.body.style.backgroundColor="black";
+        hOnes = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, a");
         hOnes.forEach(changeToDark);
         darkmodeImage.src='static/images/lightmode.png';
     }
-    else{
-        document.body.style.backgroundColor="white";
-        hOnes.forEach(darkToLight);
-        darkmodeImage.src='static/images/MOON.png';
-    }
+
+    function switchDarkOrLight() {
+        if (getCookie('darkmode')=='false'){
+            document.cookie = "darkmode=true";
+        }
+        else{
+            document.cookie = "darkmode=false";
+        }
+
+        hOnes = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, a");
+        if (getCookie('darkmode')=='true'){
+            document.body.style.backgroundColor="black";
+            hOnes.forEach(changeToDark);
+            darkmodeImage.src='static/images/lightmode.png';
+        }
+        else{
+            document.body.style.backgroundColor="white";
+            hOnes.forEach(darkToLight);
+            darkmodeImage.src='static/images/MOON.png';
+        }
     }
 }
